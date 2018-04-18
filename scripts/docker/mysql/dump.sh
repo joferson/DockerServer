be50308f99c82410d4136b853c3a4c36692249d3
Joferson Sodré <joferson.sodre@wpensar.com.br>
2018-04-18 13:33:32 -0300

    server

--- scripts/docker/mysql/dump.sh
+++ scripts/docker/mysql/dump.sh
@@ -1,1 +1,3 @@
-mysql -u root -pverysecret < $HOME/database_schema.sql 
+#!/bin/bash
+ /usr/bin/mysqld_safe --skip-grant-tables &
+ /etc/init.d/mysql start && sleep 5 && mysql -u root  < database_schema.sql
